892d599788b93d894d7242519a58fa55801ac939
kimdeokhyeon <tosico85@gmail.com>
2023-06-29 14:00:45 +0900

    test

--- services/apiService.js
+++ services/apiService.js
@@ -7,6 +7,14 @@ export const callServer = async (path, params) => {
   try {
     const { data, status, statusText } = await axios.post(requestUrl, params, {
       //const result = await axios.post(requestUrl, params, {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      },
       withCredentials: true,
     });
     if (status === 200) {
