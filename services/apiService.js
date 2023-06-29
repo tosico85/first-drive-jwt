9473965b6fbe1a8583dc84a63f103fe74e70f5c3
kimdeokhyeon <tosico85@gmail.com>
2023-06-29 22:24:35 +0900

    cors test

--- services/apiService.js
+++ services/apiService.js
@@ -5,8 +5,8 @@ export const callServer = async (path, params) => {
   const requestUrl = `${apiPaths.baseUrl}${path}`;
   let result = {};
   try {
-    const { data, status, statusText } =
-      /* await axios.post(requestUrl, params, {
+    //const { data, status, statusText } =
+    /* await axios.post(requestUrl, params, {
       //const result = await axios.post(requestUrl, params, {
       method: "POST",
       headers: {
@@ -18,7 +18,7 @@ export const callServer = async (path, params) => {
       },
       withCredentials: true,
     }); */
-      await axios({
+    /* await axios({
         method: "post",
         url: requestUrl,
         data: params,
@@ -28,13 +28,28 @@ export const callServer = async (path, params) => {
         },
         withCredentials: true,
         credentials: "include",
-      });
+      }); */
+    const response = await fetch(requestUrl, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "include", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(params), // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
+    console.log(response);
 
-    if (status === 200) {
-      result = data;
+    if (response.status === 200) {
+      result = json;
       //console.log(result);
     } else {
-      result = { result: statusText };
+      result = { result: response.statusText };
     }
   } catch (error) {
     console.log(error);
