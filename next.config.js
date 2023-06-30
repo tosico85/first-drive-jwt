a606e1848b4bd32c5074af5b986c6f2f2d3aa73f
kimdeokhyeon <tosico85@gmail.com>
2023-06-30 17:13:41 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
     API_JUSO_URL: process.env.API_JUSO_URL,
     API_JUSO_KEY: process.env.API_JUSO_KEY,
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://6corps.co.kr/:path*", // 프록시 대상 URL 설정
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
