e7064eabace9172ddcf2180c5a5d6c2de9e90b84
kimdeokhyeon <tosico85@gmail.com>
2023-07-14 03:33:10 +0900

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       },
       colors: {
         rectangleHeader: "#89C0F8",
+        layoutHeader: "#1C449C",
         richBlack: "#001219",
         midnightGreen: "#005F73",
         darkCyan: "#0A9396",
