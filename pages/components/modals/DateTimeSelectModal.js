4500bddae0eda8ff07350369eab7c33223452f29
tosico85 <tosico85@gmail.com>
2023-07-20 00:03:59 +0900

    디바이스 기준 사이즈 변경

--- pages/components/modals/DateTimeSelectModal.js
+++ pages/components/modals/DateTimeSelectModal.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; // css import
 import CustomCalendar from "../custom/Calendar";
+import DateInput from "../custom/DateInput";
 
 const DateTimeSelectModal = () => {
   const [dateValue, setDateValue] = useState(new Date());
@@ -15,6 +16,7 @@ const DateTimeSelectModal = () => {
   return (
     <div className="flex flex-col items-center">
       <CustomCalendar value={dateValue} onChange={setDateValue} />
+      <input type="text" />
     </div>
   );
 };
