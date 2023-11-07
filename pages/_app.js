fac32673f909700207cb238473a6a0c52d0cc43c
kimdeokhyeon <tosico85@gmail.com>
2023-11-07 19:39:37 +0900

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import App from "next/app";
 import { AuthProvider } from "./context/authContext";
 import Layout from "./components/Layout";
 import "../styles/globals.css";
 import "react-datepicker/dist/react-datepicker.css";
 import Modal from "react-modal";
+import loadingGif from "./loading.gif"; // GIF 이미지 파일 경로를 적절하게 수정
 
 // Set app element for react-modal
 Modal.setAppElement("#__next");
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+  state = {
+    isLoading: true, // 초기에 로딩 중 상태
+  };
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
+  async componentDidMount() {
+    // 데이터를 비동기로 불러오는 작업 예시
+    // 이 예시에서는 2초 후에 로딩 상태를 false로 변경
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
   }
 
   render() {
     const { Component, pageProps } = this.props;
+    const { isLoading } = this.state;
 
     return (
       <AuthProvider>
         <Layout>
-          <Component {...pageProps} />
+          {isLoading ? (
+            // 로딩 중일 때 GIF 이미지를 표시하고 가운데 정렬
+            <div className="h-screen flex items-center justify-center bg-black bg-opacity-0 fixed top-100 left-0 right-0 bottom-0 z-50">
+              <img
+                src="/cars/loading_9.gif"
+                alt="Loading GIF"
+                className="w-16 h-16 object-contain"
+              />
+            </div>
+          ) : (
+            // 로딩이 끝나면 페이지 컴포넌트를 표시
+            <Component {...pageProps} />
+          )}
         </Layout>
       </AuthProvider>
     );
