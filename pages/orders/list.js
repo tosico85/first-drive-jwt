c54975f34b8f8c2f641cc7bafc370c3b8b1d4fde
kimdeokhyeon <tosico85@gmail.com>
2024-08-16 15:36:49 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1229,6 +1229,7 @@ const CargoList = () => {
                                 "로지스뷰",
                                 "로지뷰",
                                 "두영",
+                                "주용테크",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
                                 ? "animate-blink text-gray-500"
@@ -1248,7 +1249,7 @@ const CargoList = () => {
                             </div>
                           )}
 
-                          {["여우별", "두영"].some((word) =>
+                          {["여우별", "두영", "주용테크"].some((word) =>
                             endCompanyName.includes(word)
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
