2825645e5c8254c093d3f73531bf84a45857054f
kimdeokhyeon <tosico85@gmail.com>
2024-09-09 20:45:16 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1164,6 +1164,7 @@ const CargoList = () => {
                                 "씨엔택",
                                 "제이앤텍",
                                 "두영",
+                                "씨앤피코리아",
                               ].some((word) => startCompanyName.includes(word))
                                 ? "animate-blink text-gray-500"
                                 : "text-gray-500"
@@ -1190,7 +1191,7 @@ const CargoList = () => {
                             </div>
                           )}
 
-                          {["두영"].some((word) =>
+                          {["두영", "씨앤피코리아"].some((word) =>
                             startCompanyName.includes(word)
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
@@ -1234,6 +1235,7 @@ const CargoList = () => {
                                 "두영",
                                 "씨앤씨인터내셔널",
                                 "주용테크",
+                                "씨앤피코리아",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
                                 ? "animate-blink text-gray-500"
@@ -1261,7 +1263,7 @@ const CargoList = () => {
                             </div>
                           )}
 
-                          {["두영"].some((word) =>
+                          {["두영", "씨앤피코리아"].some((word) =>
                             endCompanyName.includes(word)
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
