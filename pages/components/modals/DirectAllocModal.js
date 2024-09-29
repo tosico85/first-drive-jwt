24e35f0e1b5b1e39883e4d731c95587a57b22c01
kimdeokhyeon <tosico85@gmail.com>
2024-09-29 16:06:49 +0900

    chage_user

--- pages/components/modals/DirectAllocModal.js
+++ pages/components/modals/DirectAllocModal.js
@@ -131,7 +131,7 @@ const DirectAllocModal = ({ onCancel, onComplete, paramObj: cargoOrder }) => {
       endPlanDt: cargoOrder.endPlanDt,
       endCompanyName: cargoOrder.endCompanyName,
       create_user: cargoOrder.create_user,
-      userEmail: userInfo.email, // userInfo.email 확인 후 적용
+      userEmail: userInfo.userEmail, // userInfo.email 확인 후 적용
     };
 
     const result = await requestServer(apiPaths.adminDirectAlloc, paramObj);
