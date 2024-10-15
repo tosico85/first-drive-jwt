24b607d99c961a52d6ab589c517963706605151f
kimdeokhyeon <tosico85@gmail.com>
2024-10-15 14:51:31 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1255,6 +1255,7 @@ const CargoList = () => {
                                 "씨앤피코리아",
                                 "피에프네이처",
                                 "제이팩토리",
+                                "소경",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
                                 ? "animate-blink text-gray-500"
@@ -1287,6 +1288,14 @@ const CargoList = () => {
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
                               수작업/지게차 없음/1.4톤 이하 차량만 입차가능
+                            </div>
+                          )}
+
+                          {["소경"].some((word) =>
+                            endCompanyName.includes(word)
+                          ) && (
+                            <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
+                              수작업/지게차 없음
                             </div>
                           )}
 
