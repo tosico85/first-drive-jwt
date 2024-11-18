69eebd080b87438ed567155afe0966075c0f237a
kimdeokhyeon <tosico85@gmail.com>
2024-11-18 16:34:43 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1265,6 +1265,7 @@ const CargoList = () => {
                                 "메가코스",
                                 "주연테크",
                                 "인터코스",
+                                "지디케이",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
                                 ? "animate-blink text-gray-500"
@@ -1297,6 +1298,14 @@ const CargoList = () => {
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
                               수작업/지게차 없음/1.4톤 이하 차량만 입차가능
+                            </div>
+                          )}
+
+                          {["지디케이"].some((word) =>
+                            endCompanyName.includes(word)
+                          ) && (
+                            <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
+                              4시 이전도착/10초만 늦어도 하차불가
                             </div>
                           )}
 
