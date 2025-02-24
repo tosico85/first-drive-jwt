4046170c44287978df66d08c578aaf82d105d318
kimdeokhyeon <tosico85@gmail.com>
2025-02-24 17:11:36 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1272,6 +1272,7 @@ const CargoList = () => {
                                 "디킨코스메틱",
                                 "CS코리아",
                                 "한국화장품",
+                                "미페이스",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
                                 ? "animate-blink text-gray-500"
@@ -1312,6 +1313,14 @@ const CargoList = () => {
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
                               수작업/지게차 없음/1.4톤 이하 차량만 입차가능
+                            </div>
+                          )}
+
+                          {["미페이스"].some((word) =>
+                            endCompanyName.includes(word)
+                          ) && (
+                            <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
+                              컨베이어 수작업 하차
                             </div>
                           )}
 
