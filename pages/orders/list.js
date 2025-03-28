77dea38edca3a6e814c8267a32dd3fdf869a1d5f
kimdeokhyeon <tosico85@gmail.com>
2025-03-28 14:47:28 +0900

    list

--- pages/orders/list.js
+++ pages/orders/list.js
@@ -1285,6 +1285,7 @@ const CargoList = () => {
                                 "디킨코스메틱",
                                 "CS코리아",
                                 "한국화장품",
+                                "엔에프씨",
                                 "페이스라인",
                               ].some((word) => endCompanyName.includes(word)) &&
                               endCompanyName !== "미래엔코스메틱"
@@ -1326,6 +1327,14 @@ const CargoList = () => {
                           ) && (
                             <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
                               수작업/지게차 없음/1.4톤 이하 차량만 입차가능
+                            </div>
+                          )}
+
+                          {["엔에프씨"].some((word) =>
+                            endCompanyName.includes(word)
+                          ) && (
+                            <div className="absolute left-0 bottom-full mb-2 hidden w-64 p-2 bg-gray-200 text-gray-800 text-sm rounded shadow-lg group-hover:block">
+                              수작업(파렛까대기)/검수있음
                             </div>
                           )}
 
